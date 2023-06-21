79aa8fc62fe6e24b0263117c25a990ad0dea9165
Vats4065 <134689792+Vats4065@users.noreply.github.com>
2023-06-21 06:05:53 -0700

    data

---
+++ js/items.js
@@ -0,0 +1,249 @@
+items = [
+    drinks = [
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/100501.jpg",
+            title: "Java Chip Frappuccino",
+            description: "We blend mocha sauce and Frappuccino® chips with Frappuccino...",
+            price: "₹ 367.50"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/100433.jpg",
+            title: "Caffe Americano",
+            description: "Rich in flavour, full-bodied espresso with hot water in true...",
+            price: "₹ 262.50"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/105468.jpg",
+            title: "Cold coffee",
+            description: "Our signature rich in flavour espresso blended with delicate...",
+            price: "₹ 362.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/100419.jpg",
+            title: "Cappuccino",
+            description: "Our signature rich in flavour espresso blended with delicate...",
+            price: "₹ 362.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113842.jpg",
+            title: "Belgium Chocolate Latte",
+            description: "Espresso with richly flavoured Belgium chocolate sauce, moch...",
+            price: "₹ 414.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113846.jpg",
+            title: "Iced Belgium Chocolate Latte",
+            description: "Espresso with richly flavoured Belgium chocolate sauce, moch...",
+            price: "₹ 446"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109887.jpg",
+            title: "Salted Dark Caramel Latte",
+            description: "A delicious mix of our bold espresso shot, perfectly steamed.",
+            price: "₹ 414.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109891.jpg",
+            title: "Iced Salted Dark Caramel Latte",
+            description: "A delicious mix of our bold espresso shot, perfectly steamed.",
+            price: "₹ 446.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/100433.jpg",
+            title: "Caffe Americano",
+            description: "Rich in flavour, full-bodied espresso with hot water in true....",
+            price: "₹ 246.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/100419.jpg",
+            title: "Cappuccino",
+            description: "Our signature rich in flavour espresso blended with delicate...",
+            price: "₹ 362.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113842.jpg",
+            title: "Belgium Chocolate Latte",
+            description: "Espresso with richly flavoured Belgium chocolate sauce, moch...",
+            price: "₹ 414.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113846.jpg",
+            title: "Iced Belgium Chocolate Latte",
+            description: "Espresso with richly flavoured Belgium chocolate sauce, moch...",
+            price: "₹ 446"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109887.jpg",
+            title: "Salted Dark Caramel Latte",
+            description: "A delicious mix of our bold espresso shot, perfectly steamed.",
+            price: "₹ 414.75"
+        },
+
+    ],
+
+    food = [
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/104108.jpg",
+            title: "Chilli Cheese Toast",
+            description: "Cheesy and fiery snack on a toasted French-style baguette, t..",
+            price: "₹ 346.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112615.jpg",
+            title: "Tomato Basil Bruschetta",
+            description: "A diced tomato & bocconccini salad with basil & olives on a...",
+            price: "₹ 336.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112589.jpg",
+            title: "Vegan Sausage Croissant Roll",
+            description: "A diced tomato & bocconccini salad with basil & olives on a...",
+            price: "₹ 386.75"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112590.jpg",
+            title: "Vegan Croissant Bun",
+            description: "A diced tomato & bocconccini salad with basil & olives on a...",
+            price: "₹ 388.50"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112591.jpg",
+            title: "Vegan Hummus Kebab Wrap",
+            description: "Relish a vegan kebab, green peas hummus and pickled carrots ...",
+            price: "₹ 388.50"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113946.jpg",
+            title: "Chatpata Paratha Wrap",
+            description: "Tangy paneer and vegetable filling wrapped in spinach parath..",
+            price: "₹ 341.25"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113942.jpg",
+            title: "Spiced Paneer Sandwich in Focaccia",
+            description: "Freshly Assembled Sandwich An Indian masala paneer with toma...",
+            price: "₹ 399.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113944.jpg",
+            title: "Pesto & Mozzarella Sandwich in Panini",
+            description: "Freshly Assembled Sandwich A panini bread loaded with spinac...",
+            price: "₹ 414.00"
+        },
+
+    ],
+    mugs = [
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113650_1.jpg",
+            title: "Ceramic Pink and White Mug - 295ml",
+            description: "Ceramic hand painted mug with glossy glaze interior and regu...",
+            price: "₹ 1500.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/113713.jpg",
+            title: "Savannah Animals Mug - 295ml",
+            description: "Ceramic hand painted mug with glossy glaze interior and regu...",
+            price: "₹ 2190.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112804.jpg",
+            title: "Mint Blue Coral Reef Ceramic Mug (89 Ml)",
+            description: "An all mint blue demi mug with the classic embossed iconic s...",
+            price: "₹ 850.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112812.jpg",
+            title: "Siren On Coral Reef Ceramic Mug (355 Ml)",
+            description: "Experience the iridescent look of the siren on coral reef an...",
+            price: "₹ 1700.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/111941.jpg",
+            title: "Green Dekstop Stainless Steel Mug (355 Ml)",
+            description: "A desktop mug featuring a painting of artistic leaves. A pre...",
+            price: "₹ 1950.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109710.jpg",
+            title: "Hyderabad Been There Ceramic Cup (414 Ml)",
+            description: "Inspired by beautiful Indian city Hyderabad, enjoy your dail...",
+            price: "₹ 750.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/105531.jpg",
+            title: "Iconic Siren Ceramic Mug (355 Ml)",
+            description: "An all white mug with the classic iconic siren logo with an ...",
+            price: "₹ 850.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109297.jpg",
+            title: "Iconic Siren Demi Ceramic Mug (89 Ml)",
+            description: "An all white mug with the classic iconic siren logo with an ...",
+            price: "₹ 600.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109761.jpg",
+            title: "India Been There Ceramic Cup (355 Ml)",
+            description: "Inspired by beautiful Indian cities, enjoy your daily coffee...",
+            price: "₹ 1400.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112804.jpg",
+            title: "Mint Blue Coral Reef Ceramic Mug (89 Ml)",
+            description: "An all mint blue demi mug with the classic embossed iconic s...",
+            price: "₹ 850.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/112812.jpg",
+            title: "Siren On Coral Reef Ceramic Mug (355 Ml)",
+            description: "Experience the iridescent look of the siren on coral reef an...",
+            price: "₹ 1700.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/105531.jpg",
+            title: "Iconic Siren Ceramic Mug (355 Ml)",
+            description: "An all white mug with the classic iconic siren logo with an ...",
+            price: "₹ 850.00"
+        },
+
+        {
+            img: "https://starbucks-cdn-01.s3.ap-south-1.amazonaws.com/Items/Small/109297.jpg",
+            title: "Iconic Siren Demi Ceramic Mug (89 Ml)",
+            description: "An all white mug with the classic iconic siren logo with an ...",
+            price: "₹ 600.00"
+        },
+
+
+
+    ]
+]
